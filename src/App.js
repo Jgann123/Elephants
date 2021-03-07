0c0b48b8cf1ec46439bfefc074f4d89d3a6b39ba
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2021-03-07 00:39:12 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
-
-import { useState, useEffect} from 'react';
-import './App.css';
-import db from './firebase';
+import { useState, useEffect } from "react";
+import "./App.css";
+import db from "./firebase";
 //import reactDom from "react-dom";
 //import styled from 'styled-components';
-import ElephantsGal from './components/ElephantsGal';
-
-
+import ElephantsGal from "./components/ElephantsGal";
 
 function App() {
-
   const [Elephants, setElephants] = useState([]);
-   
+
   const getElephants = () => {
-
-    db.collection('Elephants').onSnapshot((snapshot) => {
-      setElephants(snapshot.docs.map ((doc) => {
-        return { id: doc.id, image1: doc.data().image1, image22: doc.data().image22 }
-      }
-      
-      ))
-    })
-
-  }
+    db.collection("Elephants").onSnapshot((snapshot) => {
+      setElephants(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            image1: doc.data().image1,
+            image22: doc.data().image22,
+          };
+        })
+      );
+    });
+  };
 
   useEffect(() => {
     getElephants();
-  }, [])
+  }, []);
 
   console.log(Elephants);
-  
 
   return (
-  <div className="App">
-  <ElephantsGal Elephants = {Elephants}/>
- 
+    <div className="App">
+      <ElephantsGal Elephants={Elephants} />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
--- src/components/ElephantsGal.js
+++ src/components/ElephantsGal.js
@@ -1,58 +1,47 @@
+import React from "react";
+import styled from "styled-components";
 
-import React from 'react';
-import styled from 'styled-components';
+const ElephantsGal = (props) => {
+  return (
+    <ElephantsList>
+      {props.Elephants.map((item) => (
+        <Channel>
+          <img src={item.image1} alt="" />
+          <img src={item.image22} alt="" />
+        </Channel>
+      ))}
+    </ElephantsList>
+  );
+};
 
-const ElephantsGal = (props)  => {
-    return (
-        <ElephantsList>
-        {props.Elephants.map(item => (
-          <Channel>
-          
-              <img src={item.image1} alt=""/>
-              <img src={item.image22} alt=""/>
-            
-          </Channel>
-        ))}
-      </ElephantsList>
-    )
-}
-
-export default ElephantsGal
-
+export default ElephantsGal;
 
 const ElephantsList = styled.div`
- align-items: center;
- padding: 250px;
- display: flex;
- justify-items: center;
- justify-content: space-between
- 
-
-`
+  align-items: center;
+  padding: 250px;
+  display: flex;
+  justify-items: center;
+  justify-content: space-between;
+`;
 
 const Channel = styled.div`
-width: 250px;
-height: 250px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-box-shadow: inset 5em 1em yellow ;
-border-radius: 25px;
+  width: 250px;
+  height: 250px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: inset 5em 1em yellow;
+  border-radius: 25px;
 
-
- img {
+  img {
     width: 350px;
     height: 200px;
     border-radius: 30px;
     display: flex;
     box-shadow: 10px 10px 8px #888888;
+  }
 
-
- }
-
- img:hover {
-
+  img:hover {
     opacity: 0.3;
- }
-`
-
+  }
+`;
